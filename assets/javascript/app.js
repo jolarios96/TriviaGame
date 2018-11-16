c8af280bcf78ff48754dfe0cac6387c3c7eba84b
Jordan Larios <jolarios96@gmail.com>
2018-11-16 15:01:40 -0800

    organized js into separate functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,27 +4,36 @@
 
 $('#start').on('click', function () {
     startGame();
-    getAnswer();
 })
 
 
 function startGame() {
-    $('#start').remove();
     console.log("It's alive!");
+    var usrInput;
 
+    $('#start').remove();
+    createButtons();
+    usrInput = getUserInput();
+}
+
+function createButtons() {
+    console.log('Calling createButtons() -- --')
     var newBtn;
 
     for (var i = 0; i < 3; i++) {
+        console.log('Generating button-' + (i + 1));
         newBtn = $('<button>');
         newBtn.addClass('app-buttons');
-        newBtn.attr('id', 'btn-' + (i+1));
-        newBtn.text('Button-' + (i+1));
+        newBtn.attr('id', 'btn-' + (i + 1));
+        newBtn.text('Button-' + (i + 1));
         $('#button-container').append(newBtn);
     }
+
+    console.log('-- Buttons Created --')
 }
 
-function getAnswer(id) {
-
+function getUserInput(id) {
+    console.log('Calling getUserInput')
     $('#button-container').on('click', '#btn-1', function () {
         console.log($(this).attr('id'));
         id = $(this).attr('id');
