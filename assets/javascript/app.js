ff5c249dcba7f0ebf32acd6831e3fd2f289f1fa0
Jordan Larios <jolarios96@gmail.com>
2018-11-17 22:49:39 -0800

    styles & showAnswer screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,65 @@
+// This Script asks the user a random question from a list.
+// After the question is answered (right/wrong), remove that question from the list. 
+// Removal method @ checkAnswer()
 
 var questionData = [
     {
         question: 'THIS IS QUESTION 1',
-        answer: 'btn-1'
+        answer: 'btn-1',
+        img: 'https://via.placeholder.com/400x300'
     },
 
     {
         question: 'THIS IS QUESTION 2',
-        answer: 'btn-2'
+        answer: 'btn-2',
+        img: 'https://via.placeholder.com/400x300'
     },
     {
         question: 'THIS IS QUESTION 3',
-        answer: 'btn-3'
-    }
+        answer: 'btn-3',
+        img: 'https://via.placeholder.com/400x300'
+    },
+    {
+        question: 'THIS IS QUESTION 4',
+        answer: 'btn-4',
+        img: 'https://via.placeholder.com/400x300'
+    },
+    {
+        question: 'THIS IS QUESTION 5',
+        answer: 'btn-1',
+        img: 'https://via.placeholder.com/400x300'
+    },
+    // {
+    //     question: 'THIS IS QUESTION 6',
+    //     answer: 'btn-2',
+    //     img: 'https://via.placeholder.com/400x300'
+    // },
+    // {
+    //     question: 'THIS IS QUESTION 7',
+    //     answer: 'btn-3',
+    //     img: 'https://via.placeholder.com/400x300'
+    // },
+    // {
+    //     question: 'THIS IS QUESTION 8',
+    //     answer: 'btn-4',
+    //     img: 'https://via.placeholder.com/400x300'
+    // },
+    // {
+    //     question: 'THIS IS QUESTION 9',
+    //     answer: 'btn-1',
+    //     img: 'https://via.placeholder.com/400x300'
+    // },
+    // {
+    //     question: 'THIS IS QUESTION 10',
+    //     answer: 'btn-2',
+    //     img: 'https://via.placeholder.com/400x300'
+    // },
 ]
 
 
 $('#start').on('click', function () {
+    $('#start').remove();
+    $('#title').empty();
     startGame();
 })
 
@@ -26,45 +69,39 @@ function startGame() {
     var usrInput;
     var index;
 
-    $('#start').remove();
-    $('#title').remove();
-
     // TIMER FUNCTIONS HERE
 
     ////
-
     index = Math.floor(Math.random() * questionData.length);
     console.log(questionData[index].question);
     $('#question').text(questionData[index].question);
 
+
     getButtons();
 
     // on-click events
+    console.log('Waiting for button push . . .');
     $('#button-container').on('click', '#btn-1', function () {
-        console.log($(this).attr('id'));
         buttonID = $(this).attr('id');
-        console.log('id = ' + buttonID);
+        console.log('Button Pushed: ' + buttonID);
         checkAnswer(buttonID, index);
     });
 
     $('#button-container').on('click', '#btn-2', function () {
-        console.log($(this).attr('id'));
         buttonID = $(this).attr('id');
-        console.log('id = ' + buttonID);
+        console.log('Button Pushed: ' + buttonID);
         checkAnswer(buttonID, index);
     });
 
     $('#button-container').on('click', '#btn-3', function () {
-        console.log($(this).attr('id'));
         buttonID = $(this).attr('id');
-        console.log('id = ' + buttonID);
+        console.log('Button Pushed: ' + buttonID);
         checkAnswer(buttonID, index);
     });
 
     $('#button-container').on('click', '#btn-4', function () {
-        console.log($(this).attr('id'));
         buttonID = $(this).attr('id');
-        console.log('id = ' + buttonID);
+        console.log('Button Pushed: ' + buttonID);
         checkAnswer(buttonID, index);
     });
 
@@ -94,11 +131,36 @@ function getButtons() {
 
 // UNFINISHED
 function checkAnswer(buttonID, index) {
-    console.log(questionData[index].answer);
+    console.log('Actual Answer: ' + questionData[index].answer);
     if (buttonID === questionData[index].answer) {
         console.log('answer correct');
+        
+
+
+        showAnswer(index);
+
+        // Remove question from questionData[]
+        questionData.splice(index, 1);
+        console.log(questionData);
     }
     else {
-        console.log('incorrect')
+        console.log('incorrect');
+
+
+        showAnswer(index);
+
+        // Remove question from questionData[]
+        questionData.splice(index, 1);
+        console.log(questionData);
     }
+}
+
+function showAnswer(index){
+    $('#question').empty();
+    $('#button-container').empty();
+
+    var newImg = $('<img>')
+    newImg.attr('id', 'answer-img');
+    newImg.attr('src', questionData[index].img)
+    $('#app').append(newImg);
 }
