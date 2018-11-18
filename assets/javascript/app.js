cc05904fc49c56fcd7177b13596635eb255579fa
Jordan Larios <jolarios96@gmail.com>
2018-11-17 19:21:39 -0800

    moved on-click events into main function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,16 @@
 var questionData = [
     {
         question: 'THIS IS QUESTION 1',
-        answer: ''
+        answer: 'btn-1'
     },
 
     {
         question: 'THIS IS QUESTION 2',
-        answer: ''
+        answer: 'btn-2'
     },
     {
         question: 'THIS IS QUESTION 3',
-        answer: ''
+        answer: 'btn-3'
     }
 ]
 
@@ -30,18 +30,44 @@ function startGame() {
     $('#title').remove();
 
     // TIMER FUNCTIONS HERE
-    
+
     ////
 
     index = Math.floor(Math.random() * questionData.length);
     console.log(questionData[index].question);
     $('#question').text(questionData[index].question);
-    
+
     getButtons();
-    usrInput = getUserInput();
-    // checkAnswer(usrInput);
-    // OR
-    // checkAnswer(getUserInput());
+
+    // on-click events
+    $('#button-container').on('click', '#btn-1', function () {
+        console.log($(this).attr('id'));
+        buttonID = $(this).attr('id');
+        console.log('id = ' + buttonID);
+        checkAnswer(buttonID, index);
+    });
+
+    $('#button-container').on('click', '#btn-2', function () {
+        console.log($(this).attr('id'));
+        buttonID = $(this).attr('id');
+        console.log('id = ' + buttonID);
+        checkAnswer(buttonID, index);
+    });
+
+    $('#button-container').on('click', '#btn-3', function () {
+        console.log($(this).attr('id'));
+        buttonID = $(this).attr('id');
+        console.log('id = ' + buttonID);
+        checkAnswer(buttonID, index);
+    });
+
+    $('#button-container').on('click', '#btn-4', function () {
+        console.log($(this).attr('id'));
+        buttonID = $(this).attr('id');
+        console.log('id = ' + buttonID);
+        checkAnswer(buttonID, index);
+    });
+
 }
 
 // RE-STYLE THIS:
@@ -55,6 +81,7 @@ function getButtons() {
         newBtn = $('<button>');
         newBtn.addClass('app-buttons centered');
         newBtn.attr('id', 'btn-' + (i + 1));
+        newBtn.attr('data-index', i);
         newBtn.text('Button-' + (i + 1));
         $('#button-container').append(newBtn);
     }
@@ -63,40 +90,15 @@ function getButtons() {
 }
 
 
-// OPTIMIZE THIS:
-// make it so the buttons call a 'checkAnswer()' function
-function getUserInput(id) {
-    console.log('Calling getUserInput')
-    $('#button-container').on('click', '#btn-1', function () {
-        console.log($(this).attr('id'));
-        id = $(this).attr('id');
-        console.log('id = ' + id);
-    });
 
-    $('#button-container').on('click', '#btn-2', function () {
-        console.log($(this).attr('id'));
-        id = $(this).attr('id');
-        console.log('id = ' + id);
-    });
-
-    $('#button-container').on('click', '#btn-3', function () {
-        console.log($(this).attr('id'));
-        id = $(this).attr('id');
-        console.log('id = ' + id);
-    });
-
-    $('#button-container').on('click', '#btn-4', function () {
-        console.log($(this).attr('id'));
-        id = $(this).attr('id');
-        console.log('id = ' + id);
-    });
-
-    return id;
-    // alternatively, call checkAnswer() from here directly?
-    // checkAnswer(id);
-}
 
 // UNFINISHED
-function checkAnswer() {
-
+function checkAnswer(buttonID, index) {
+    console.log(questionData[index].answer);
+    if (buttonID === questionData[index].answer) {
+        console.log('answer correct');
+    }
+    else {
+        console.log('incorrect')
+    }
 }
